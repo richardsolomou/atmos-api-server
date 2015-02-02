580dae83ceef5c18f3d1f86214ca8e71587365fe
Richard Solomou <rsolomou@gmail.com>
2015-02-02 19:51:30 +0000

    Added additional routes.

--- server.js
+++ server.js
@@ -63,14 +63,78 @@ var server = restify.createServer({
  *******************************/
 
 /**
+ * Retrieves the units of a specific lecturer from the database.
+ */
+server.get(prefix + '/lecturers/:lecturer_id/units', function (req, res) {
+	mysql_conn.query('SELECT * FROM `lecturers` WHERE `lecturer_id` = :lecturer_id', { lecturer_id: req.params.lecturer_id }, function (err, results) {
+		if (err) return res.send(err);
+		if (!results || !results.length) {
+			err = new Error('Invalid lecturer ID.');
+			err.status = 404;
+			return res.send(err);
+		}
+
+		mysql_conn.query('SELECT `units`.* FROM `lecturersessions` INNER JOIN `sessions` ON `lecturersessions`.`session_id` = `sessions`.`session_id` INNER JOIN `units` ON `units`.`unit_id` = `sessions`.`unit_id` WHERE `lecturersessions`.`lecturer_id` = :lecturer_id GROUP BY `units`.`unit_id`', { lecturer_id: req.params.lecturer_id }, function (err, results) {
+			if (err) return res.send(err);
+
+			return res.send(results);
+		});
+	});
+});
+
+/**
+ * Retrieves the sessions of a specific lecturer from the database.
+ */
+server.get(prefix + '/lecturers/:lecturer_id/sessions', function (req, res) {
+	mysql_conn.query('SELECT * FROM `lecturers` WHERE `lecturer_id` = :lecturer_id', { lecturer_id: req.params.lecturer_id }, function (err, results) {
+		if (err) return res.send(err);
+		if (!results || !results.length) {
+			err = new Error('Invalid lecturer ID.');
+			err.status = 404;
+			return res.send(err);
+		}
+
+		mysql_conn.query('SELECT `sessions`.* FROM `lecturersessions` INNER JOIN `sessions` ON `lecturersessions`.`session_id` = `sessions`.`session_id` WHERE `lecturersessions`.`lecturer_id` = :lecturer_id', { lecturer_id: req.params.lecturer_id }, function (err, results) {
+			if (err) return res.send(err);
+
+			return res.send(results);
+		});
+	});
+});
+
+/**
+ * Retrieves a specific lecturer from the database.
+ */
+server.get(prefix + '/lecturers/:lecturer_id', function (req, res) {
+	mysql_conn.query('SELECT * FROM `lecturers` WHERE `lecturer_id` = :lecturer_id', { lecturer_id: req.params.lecturer_id }, function (err, results) {
+		if (err) return res.send(err);
+		if (!results || !results.length) {
+			err = new Error('Invalid lecturer ID.');
+			err.status = 404;
+			return res.send(err);
+		}
+
+		return res.send(results[0]);
+	});
+});
+
+/**
+ * Retrieves a list of lecturers from the database.
+ */
+server.get(prefix + '/lecturers', function (req, res) {
+	mysql_conn.query('SELECT * FROM `lecturers`', function (err, results) {
+		if (err) return res.send(err);
+
+		return res.send(results);
+	});
+});
+
+/**
  * Retrieves the attendance log of a specific student from the database.
  */
 server.get(prefix + '/students/:student_id/attendance', function (req, res) {
 	mysql_conn.query('SELECT * FROM `students` WHERE `student_id` = :student_id', { student_id: req.params.student_id }, function (err, results) {
-		if (err) {
-			return res.send(err);
-		}
-
+		if (err) return res.send(err);
 		if (!results || !results.length) {
 			err = new Error('Invalid student ID.');
 			err.status = 404;
@@ -78,9 +142,7 @@ server.get(prefix + '/students/:student_id/attendance', function (req, res) {
 		}
 
 		mysql_conn.query('SELECT `session_id`, `attendance_recorded` FROM `attendance` WHERE `student_id` = :student_id', { student_id: req.params.student_id }, function (err, results) {
-			if (err) {
-				return res.send(err);
-			}
+			if (err) return res.send(err);
 
 			return res.send(results);
 		});
@@ -92,10 +154,7 @@ server.get(prefix + '/students/:student_id/attendance', function (req, res) {
  */
 server.get(prefix + '/students/:student_id', function (req, res) {
 	mysql_conn.query('SELECT * FROM `students` WHERE `student_id` = :student_id', { student_id: req.params.student_id }, function (err, results) {
-		if (err) {
-			return res.send(err);
-		}
-
+		if (err) return res.send(err);
 		if (!results || !results.length) {
 			err = new Error('Invalid student ID.');
 			err.status = 404;
@@ -111,15 +170,7 @@ server.get(prefix + '/students/:student_id', function (req, res) {
  */
 server.get(prefix + '/students', function (req, res) {
 	mysql_conn.query('SELECT * FROM `students`', function (err, results) {
-		if (err) {
-			return res.send(err);
-		}
-
-		if (!results || !results.length) {
-			err = new Error('Table is empty.');
-			err.status = 404;
-			return res.send(err);
-		}
+		if (err) return res.send(err);
 
 		return res.send(results);
 	});
