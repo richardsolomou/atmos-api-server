3d838c6184d35767724abc4fab00cab9e3d1a0df
Richard Solomou <rsolomou@gmail.com>
2015-02-04 19:03:20 +0000

    Added dependencies, field population, and an update lecturer route.

--- routes/lecturers.js
+++ routes/lecturers.js
@@ -1,4 +1,19 @@
 module.exports = function (server, mysql_conn, prefix, restify) {
+	// Load module dependencies.
+	var async = require('async'),
+		winston = require('winston');
+
+	// Setup logging module.
+	var logger = new winston.Logger({
+		transports: [
+			new winston.transports.File({ filename: require('path').dirname(require.main.filename) + '/logs/debug.log' })
+		],
+		exceptionHandlers: [
+			new winston.transports.File({ filename: require('path').dirname(require.main.filename) + '/logs/exceptions.log' })
+		],
+		exitOnError: false
+	});
+
 	/**
 	 * Retrieves the units of a specific lecturer from the database.
 	 */
@@ -26,7 +41,18 @@ module.exports = function (server, mysql_conn, prefix, restify) {
 			mysql_conn.query('SELECT `sessions`.* FROM `lecturersessions` INNER JOIN `sessions` ON `lecturersessions`.`session_id` = `sessions`.`session_id` WHERE `lecturersessions`.`lecturer_id` = :lecturer_id', { lecturer_id: req.params.lecturer_id }, function (err, results) {
 				if (err) return next(err);
 
-				return res.send(results);
+				if (req.query.populate && req.query.populate == 'unit_id') {
+					async.each(results, function (session, callback) {
+						mysql_conn.query('SELECT * FROM `units` WHERE `unit_id` = :unit_id', { unit_id: session.unit_id }, function (err, units) {
+							session.unit_id = units[0];
+							callback();
+						});
+					}, function () {
+						return res.send(results);
+					});
+				} else {
+					return res.send(results);
+				}
 			});
 		});
 	});
@@ -40,6 +66,17 @@ module.exports = function (server, mysql_conn, prefix, restify) {
 			if (!results || !results.length) return next(new restify.errors.NotFoundError('Invalid lecturer ID.'));
 
 			return res.send(results[0]);
+		});
+	});
+
+    /**
+     * Updates a specific lecturer.
+     */
+	server.put(prefix + '/lecturers/:lecturer_id', function (req, res, next) {
+		mysql_conn.query('UPDATE `lecturers` SET `lecturer_name` = :lecturer_name, `lecturer_email` = :lecturer_email WHERE `lecturer_id` = :lecturer_id', { lecturer_id: req.params.lecturer_id, lecturer_name: req.params.lecturer_name, lecturer_email: req.params.lecturer_email }, function (err, results) {
+			if (err) return next(err);
+			
+			return res.send(results);
 		});
 	});
 
